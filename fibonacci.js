60aed54dbf3da1cee01c130052cf45375527f3f9
Errick Basa-Denis <errickbd@gmail.com>
2024-01-31 16:08:04 -0600

    finished solution and test

--- fibonacci.js
+++ fibonacci.js
@@ -1,5 +1,30 @@
 function fibonacci(num) {
-  return 0;
+  let numOne = 0
+  let numTwo = 1
+  let countIndex = 2
+  let nextNum = 0
+  
+  while (countIndex <= num){
+    if (num === 0){
+      return 0
+    }
+    else if (num === 1){
+      return 1
+    }
+
+    nextNum = numOne + numTwo;
+    numOne = numTwo;
+    numTwo = nextNum;
+    
+    countIndex++
+
+  }
+  
+  return nextNum;
+  
 }
+
+console.log(fibonacci(1))
+
 
 module.exports = fibonacci;
